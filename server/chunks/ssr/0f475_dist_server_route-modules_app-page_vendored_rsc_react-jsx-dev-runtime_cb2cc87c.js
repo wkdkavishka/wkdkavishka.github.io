8f6cd1d73802e125efd4d7c7664a2e8b364f7719
wkdkavishka <w.k.dumindukavishka@gmail.com>
2025-07-27 16:02:10 +0530

    auto deploy 2025-07-27 16:02:10

---
+++ server/chunks/ssr/0f475_dist_server_route-modules_app-page_vendored_rsc_react-jsx-dev-runtime_cb2cc87c.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+"[project]/node_modules/next/dist/server/route-modules/app-page/vendored/rsc/react-jsx-dev-runtime.js [app-rsc] (ecmascript)": (function(__turbopack_context__) {
+
+var { g: global, __dirname, m: module, e: exports } = __turbopack_context__;
+{
+"use strict";
+module.exports = __turbopack_context__.r("[project]/node_modules/next/dist/server/route-modules/app-page/module.compiled.js [app-rsc] (ecmascript)").vendored['react-rsc'].ReactJsxDevRuntime; //# sourceMappingURL=react-jsx-dev-runtime.js.map
+}}),
+
+};
+
+//# sourceMappingURL=0f475_dist_server_route-modules_app-page_vendored_rsc_react-jsx-dev-runtime_cb2cc87c.js.map
