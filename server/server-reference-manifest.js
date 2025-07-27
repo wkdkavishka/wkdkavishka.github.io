1f69111a3f00e223934d124d4ef58986a6a4d2ac
wkdkavishka <w.k.dumindukavishka@gmail.com>
2025-07-27 22:04:17 +0530

    auto deploy 2025-07-27 22:04:17

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"y0YZmYRPtrCwpoWnCktA2/3m7H9VX9FNLy6Dd0UmTVM=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cXKEBZTx4+9pnFzbre5vguWng0aoDJDcJyRkgNn7y2k=\"\n}"
