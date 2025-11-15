643f64d236c238d672500d9a2f67fea31fdc5cfa
wkdkavishka <w.k.dumindukavishka@gmail.com>
2025-11-16 00:16:50 +0530

    auto deploy 2025-11-15T18:46:50.067Z

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // public/sw.js
 //! CACHE_NAME is updated to the date and time
 //! this must be 4th line of the file
-const CACHE_NAME = `pwa-cache-2025-11-15T18:41:12.992Z`;
+const CACHE_NAME = `pwa-cache-2025-11-15T18:46:34.731Z`;
 const OFFLINE_PAGE = "/offline.html";
 
 const PRECACHE_ASSETS = [
